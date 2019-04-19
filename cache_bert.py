b0b6a4bd0db4084963bddf340588bf9d7365c672
Omkar <omkarvdamle13@gmail.com>
2019-04-19 18:49:30 -0400

    cache_bert updated - need to run and check some parts

--- cache_bert.py
+++ cache_bert.py
@@ -9,35 +9,191 @@ import h5py
 import json
 import sys
 
-def build_bert():
-  #token_ph = tf.placeholder(tf.string, [None, None])
-  #len_ph = tf.placeholder(tf.int32, [None])
-  BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
-  bert_module = hub.Module(BERT_MODEL_HUB)
+import bert
+
+
+OUTPUT_DIR = "/scratch/ovd208/nlu/my_e2e-coref/e2e-coref/bert_output_dir"
+
+
+def create_model(is_predicting, input_ids, input_mask, segment_ids, labels,
+                 num_labels):
+  """Creates a classification model."""
+
+  bert_module = hub.Module(
+      BERT_MODEL_HUB,
+      trainable=False)
+  bert_inputs = dict(
+      input_ids=input_ids,
+      input_mask=input_mask,
+      segment_ids=segment_ids)
+  bert_outputs = bert_module(
+      inputs=bert_inputs,
+      signature="tokens",
+      as_dict=True)
+
+  # Use "pooled_output" for classification tasks on an entire sentence.
+  # Use "sequence_outputs" for token-level output.
+  output_layer = bert_outputs["sequence_outputs"]
+
+  explore output_layer
+  hidden_size = output_layer.shape[-1].value
+
+  # If we're predicting, we want predicted labels and the probabiltiies.
+  if is_predicting:
+    return (output_layer_emb)
+
+  return (output_layer_emb)
+
+
+# model_fn_builder actually creates our model function
+# using the passed parameters for num_labels, learning_rate, etc.
+def model_fn_builder(num_labels, learning_rate, num_train_steps,
+                     num_warmup_steps):
+  """Returns `model_fn` closure for TPUEstimator."""
+  def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
+    """The `model_fn` for TPUEstimator."""
+
+    input_ids = features["input_ids"]
+    input_mask = features["input_mask"]
+    segment_ids = features["segment_ids"]
+    label_ids = features["label_ids"]
+
+    is_predicting = (mode == tf.estimator.ModeKeys.PREDICT)
     
-  #TODO update this part
+    check is_predicting is true or not
+
+    # TRAIN and EVAL
+    if not is_predicting:
 
 
-  lm_embeddings = elmo_module(
-      inputs={"tokens": token_ph, "sequence_len": len_ph},
-      signature="tokens", as_dict=True)
-  
-  word_emb = lm_embeddings["word_emb"]
-  lm_emb = tf.stack([tf.concat([word_emb, word_emb], -1),
-                     lm_embeddings["lstm_outputs1"],
-                     lm_embeddings["lstm_outputs2"]], -1)
-  return token_ph, len_ph, lm_emb
+      (loss, predicted_labels, log_probs) = create_model(
+        is_predicting, input_ids, input_mask, segment_ids, label_ids, num_labels)
+
+      train_op = bert.optimization.create_optimizer(
+          loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu=False)
+
+      # Calculate evaluation metrics. 
+      def metric_fn(label_ids, predicted_labels):
+        accuracy = tf.metrics.accuracy(label_ids, predicted_labels)
+        f1_score = tf.contrib.metrics.f1_score(
+            label_ids,
+            predicted_labels)
+        auc = tf.metrics.auc(
+            label_ids,
+            predicted_labels)
+        recall = tf.metrics.recall(
+            label_ids,
+            predicted_labels)
+        precision = tf.metrics.precision(
+            label_ids,
+            predicted_labels) 
+        true_pos = tf.metrics.true_positives(
+            label_ids,
+            predicted_labels)
+        true_neg = tf.metrics.true_negatives(
+            label_ids,
+            predicted_labels)   
+        false_pos = tf.metrics.false_positives(
+            label_ids,
+            predicted_labels)  
+        false_neg = tf.metrics.false_negatives(
+            label_ids,
+            predicted_labels)
+        return {
+            "eval_accuracy": accuracy,
+            "f1_score": f1_score,
+            "auc": auc,
+            "precision": precision,
+            "recall": recall,
+            "true_positives": true_pos,
+            "true_negatives": true_neg,
+            "false_positives": false_pos,
+            "false_negatives": false_neg
+        }
+
+      eval_metrics = metric_fn(label_ids, predicted_labels)
+
+      if mode == tf.estimator.ModeKeys.TRAIN:
+        return tf.estimator.EstimatorSpec(mode=mode,
+          loss=loss,
+          train_op=train_op)
+      else:
+          return tf.estimator.EstimatorSpec(mode=mode,
+            loss=loss,
+            eval_metric_ops=eval_metrics)
+    else:
+      (output_layer_emb) = create_model(
+        is_predicting, input_ids, input_mask, segment_ids, label_ids, num_labels)
+
+      predictions = {
+          'emb': output_layer_emb
+      }
+      return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+  # Return the actual model function in the closure
+  return model_fn
+
 
 def cache_dataset(data_path, session, token_ph, len_ph, lm_emb, out_file):
 
+  BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
+
+  def create_tokenizer_from_hub_module():
+  """Get the vocab file and casing info from the Hub module."""
+    with tf.Graph().as_default():
+      bert_module = hub.Module(BERT_MODEL_HUB)
+      tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+      with tf.Session() as sess:
+        vocab_file, do_lower_case = sess.run([tokenization_info["vocab_file"],
+                                            tokenization_info["do_lower_case"]])
+      
+    return bert.tokenization.FullTokenizer(
+      vocab_file=vocab_file, do_lower_case=do_lower_case)
+
+  tokenizer = create_tokenizer_from_hub_module()
+
+  MAX_SEQ_LENGTH = 512
+  BATCH_SIZE = 1
+
+  run_config = tf.estimator.RunConfig(model_dir=OUTPUT_DIR)
+
+  model_fn = model_fn_builder(num_labels=len(label_list), learning_rate=0.001, num_train_steps=1, num_warmup_steps = 1)    
+
+  estimator = tf.estimator.Estimator(
+    model_fn=model_fn,
+    config=run_config,
+    params={"batch_size": BATCH_SIZE})
+
+
+
+
   #TODO
-  #update this part - take care of different BERT alignment, store a map of normal alignment to BERT alignment?
-
-
+  #update this part - take care of different BERT alignment, store a map of normal alignment to BERT alignment
+ 
   with open(data_path) as in_file:
     for doc_num, line in enumerate(in_file.readlines()):
       example = json.loads(line)
       sentences = example["sentences"]
+
+      check sentences format - need a single string
+
+      bert_input = bert.run_classifier.InputExample(guid=None, text_a = sentences, text_b = None, label = 1)
+      
+      label_list = [1]
+
+      features = bert.run_classifier.convert_examples_to_features([bert_input], label_list, MAX_SEQ_LENGTH, tokenizer)
+
+      current_input_fn = run_classifier.input_fn_builder(
+        features=features,
+        seq_length=MAX_SEQ_LENGTH,
+        is_training=False,
+        drop_remainder=False)
+
+      pred = estimator.predict(current_input_fn)
+ 
+      save this pred["emb"]
+
+      """
       max_sentence_length = max(len(s) for s in sentences)
       tokens = [[""] * max_sentence_length for _ in sentences]
       text_len = np.array([len(s) for s in sentences])
@@ -56,6 +212,8 @@ def cache_dataset(data_path, session, token_ph, len_ph, lm_emb, out_file):
         group[str(i)] = e
       if doc_num % 10 == 0:
         print("Cached {} documents in {}".format(doc_num + 1, data_path))
+
+      """
 
 if __name__ == "__main__":
   token_ph, len_ph, lm_emb = build_bert()
